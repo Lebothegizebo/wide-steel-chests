8584f17c26761a8d5d92dd69dcd93b78159e8544
Lebothegizebo <daltonliam321@gmail.com>
2024-10-12 19:32:34 +1100

    updated migrations

--- migrations/migrations.lua
+++ migrations/migrations.lua
@@ -1,7 +1,8 @@
 for index, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipes = force.recipes
-  
+
+    technologies["wide-containers"].researched = technologies["wide-steel-containers"].researched
     recipes["wide-wooden-chest"].enabled = technologies["wide-containers"].researched
     recipes["wide-iron-chest"].enabled = technologies["wide-containers"].researched
     recipes["wide-steel-chest"].enabled = technologies["wide-containers"].researched
