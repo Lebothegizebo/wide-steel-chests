cad2f7d254561bf59963fed238263907a68b0120
Lebothegizebo <daltonliam321@gmail.com>
2024-10-12 18:37:01 +1100

    Updated Technologies

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,18 +1,10 @@
---[[table.insert(
-	data.raw["technology"]["steel-processing"].effects,
-	{
-		type = "unlock-recipe",
-		recipe = "wide-steel-chest"
-	}
-)
---]]
 data:extend({
 	{
 	  type = "technology",
-	  name = "wide-containers",
-	  icons = { { icon = "__wide-steel-chests__/graphics/technology/wide-containers.png", icon_size = 128 } },
+	  name = "wide-steel-containers",
+	  icons = { { icon = "__wide-steel-chests__/graphics/technology/wide-steel-containers.png", icon_size = 128 } },
 	  order = "a",
-	  prerequisites = {"steel-processing", "logistic-science-pack"},
+	  prerequisites = {"wide-containers","steel-processing", "logistic-science-pack"},
 	  unit = {
 		count = 100,
 		ingredients = {
@@ -21,6 +13,20 @@ data:extend({
 		},
 		time = 30
 	  },
-	  effects = { { type = "unlock-recipe", recipe = "wide-wooden-chest" }, { type = "unlock-recipe", recipe = "wide-iron-chest" }, { type = "unlock-recipe", recipe = "wide-steel-chest" } },
+	  effects = { { type = "unlock-recipe", recipe = "wide-steel-chest" } },
 	},
+	{
+		type = "technology",
+		name = "wide-containers",
+		icons = { { icon = "__wide-steel-chests__/graphics/technology/wide-containers.png", icon_size = 128 } },
+		order = "a",
+		unit = {
+		  count = 20,
+		  ingredients = {
+			{"automation-science-pack", 1},
+		  },
+		  time = 5
+		},
+		effects = { { type = "unlock-recipe", recipe = "wide-wooden-chest" }, { type = "unlock-recipe", recipe = "wide-iron-chest" } },
+	  },
 })
