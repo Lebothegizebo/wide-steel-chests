53d714b83e838fb4ca9fab4815c65342de599eb0
Lebothegizebo <daltonliam321@gmail.com>
2024-10-15 00:49:44 +1100

    Final Compatibility Tweak

--- prototypes/compatibility/aai-containers/number.lua
+++ prototypes/compatibility/aai-containers/number.lua
@@ -1,8 +1,8 @@
-data.raw["item"]["wide-steel-chest"].icons[2].icon_size = 40
+data.raw["item"]["wide-steel-chest"].icons[2].icon_size = 47
 data.raw["item"]["wide-steel-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
 data.raw["item"]["wide-steel-chest"].icons[2].scale = 0.5
-data.raw["item"]["wide-steel-chest"].icons[2].shift = {10, 5}
-data.raw["recipe"]["wide-steel-chest"].icons[2].icon_size = 40
+data.raw["item"]["wide-steel-chest"].icons[2].shift = {-2.5, -5.5}
+data.raw["recipe"]["wide-steel-chest"].icons[2].icon_size = 47
 data.raw["recipe"]["wide-steel-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
 data.raw["recipe"]["wide-steel-chest"].icons[2].scale = 0.5
-data.raw["recipe"]["wide-steel-chest"].icons[2].shift = {10, -5}
+data.raw["recipe"]["wide-steel-chest"].icons[2].shift = {-2.5, -5.5}
