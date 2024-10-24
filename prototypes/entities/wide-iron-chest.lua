793308df6e29e1790894cd2999c69ed0b39cd5f7
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-10-24 13:00:06 -0600

    Convert Iron chest to function

--- prototypes/entities/wide-iron-chest.lua
+++ prototypes/entities/wide-iron-chest.lua
@@ -1,56 +1,17 @@
-data:extend(
-{
- {
-      type = "item",
-      name = "wide-iron-chest",
-      subgroup = "wide-storage",
-      place_result = "wide-iron-chest",
-      stack_size = 50,
-      order = "b",
-      icons = 
-      {
-        {
-          icon = "__wide-steel-chests__/graphics/icons/wide-iron-chest.png",
-          icon_size = 128,
-        },
-      }
-  },
+make_wide_and_tall{
+  name = "iron-chest",
+  icons =
   {
-    type = "container",
-    name = "wide-iron-chest",
-    icons = 
     {
-      {
-        icon = "__wide-steel-chests__/graphics/icons/wide-iron-chest.png",
-        icon_size = 128,
-      },
+      icon = "__wide-steel-chests__/graphics/icons/wide-iron-chest.png",
+      icon_size = 128,
     },
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 0.2, result = "wide-iron-chest"},
-    max_health = 200,
-    corpse = "wide-iron-chest-remnants",
-    dying_explosion = "iron-chest-explosion",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.43 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.43 },
-    resistances =
-    {
-      {
-        type = "fire",
-        percent = 80
-      },
-      {
-        type = "impact",
-        percent = 30
-      }
-    },
-    collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
-    selection_box = {{-1, -0.5}, {1, 0.5}},
-    scale_info_icons = true,
-    fast_replaceable_group = "container",
-    inventory_size = 64,
-    vehicle_impact_sound = table.deepcopy(data.raw.container["iron-chest"].vehicle_impact_sound),
-    damaged_trigger_effect = table.deepcopy(data.raw.container["iron-chest"].damaged_trigger_effect),
-    picture =
+  },
+  collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
+  selection_box = {{-1, -0.5}, {1, 0.5}},
+  picture =
+  {
+    north =
     {
       layers =
       {
@@ -73,7 +34,28 @@ data:extend(
         }
       }
     },
-    circuit_connector = table.deepcopy(circuit_connector_definitions["wide-chest"]),
-    circuit_wire_max_distance = default_circuit_wire_max_distance,
+    east =
+    {
+      layers =
+      {
+        {
+          filename = "__wide-steel-chests__/graphics/entity/tall-iron-chest.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 80,
+          height = 128,
+          shift = util.by_pixel(-0.5, -0.25),
+        },
+        {
+          filename = "__wide-steel-chests__/graphics/entity/shadows/tall-iron-chest-shadow.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 46,
+          height = 180,
+          shift = util.by_pixel(6, 24.25-10),
+          draw_as_shadow = true,
+        }
+      }
+    }
   }
-})
+}
