6211e0cad77340acb66b56787d9d99af07877925
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-10-24 14:14:25 -0600

    Convert other chests

--- prototypes/entities/wide-steel-chest.lua
+++ prototypes/entities/wide-steel-chest.lua
@@ -1,60 +1,21 @@
-data:extend(
-{
- {
-    type = "item",
-    name = "wide-steel-chest",
-    subgroup = "wide-storage",
-    place_result = "wide-steel-chest",
-    stack_size = 50,
-    order = "c",
-    icons = 
+make_wide_and_tall{
+  name = "steel-chest",
+  icons =
+  {
     {
-      {
-        icon = "__wide-steel-chests__/graphics/icons/wide-steel-chest.png",
-        icon_size = 128,
-      },
-    }
+      icon = "__wide-steel-chests__/graphics/icons/wide-steel-chest.png",
+      icon_size = 128,
+    },
   },
-   {
-    type = "container",
-    name = "wide-steel-chest",
-    icons = 
-    {
-      {
-        icon = "__wide-steel-chests__/graphics/icons/wide-steel-chest.png",
-        icon_size = 128,
-      },
-    },
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 0.2, result = "wide-steel-chest"},
-    max_health = 350,
-    corpse = "wide-steel-chest-remnants",
-    dying_explosion = "steel-chest-explosion",
-    open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.43 },
-    close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.43 },
-    resistances =
-    {
-      {
-        type = "fire",
-        percent = 90
-      },
-      {
-        type = "impact",
-        percent = 60
-      }
-    },
-    collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
-    selection_box = {{-1, -0.5}, {1, 0.5}},
-    scale_info_icons = true,
-    fast_replaceable_group = "container",
-    inventory_size = 96,
-    vehicle_impact_sound = table.deepcopy(data.raw.container["steel-chest"].vehicle_impact_sound),
-    damaged_trigger_effect = table.deepcopy(data.raw.container["steel-chest"].damaged_trigger_effect),
-    picture =
+  collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
+  selection_box = {{-1, -0.5}, {1, 0.5}},
+  picture =
+  {
+    north =
     {
       layers =
       {
-        { 
+        {
           filename = "__wide-steel-chests__/graphics/entity/wide-steel-chest.png",
           priority = "extra-high",
           scale = 0.5,
@@ -73,7 +34,28 @@ data:extend(
         }
       }
     },
-    circuit_connector = table.deepcopy(circuit_connector_definitions["wide-chest"]),
-    circuit_wire_max_distance = default_circuit_wire_max_distance,
+    east =
+    {
+      layers =
+      {
+        {
+          filename = "__wide-steel-chests__/graphics/entity/tall-steel-chest.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 80,
+          height = 128,
+          shift = util.by_pixel(-0.5, -0.25),
+        },
+        {
+          filename = "__wide-steel-chests__/graphics/entity/shadows/tall-steel-chest-shadow.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 46,
+          height = 165,
+          shift = util.by_pixel(8, 24.25-10),
+          draw_as_shadow = true,
+        }
+      }
+    }
   }
-})
+}
--- prototypes/entities/wide-wooden-chest.lua
+++ prototypes/entities/wide-wooden-chest.lua
@@ -1,45 +1,17 @@
-data:extend(
-{
- {
-    type = "item",
-    name = "wide-wooden-chest",
-    icons = 
+make_wide_and_tall{
+  name = "wooden-chest",
+  icons =
+  {
     {
-      {
-        icon = "__wide-steel-chests__/graphics/icons/wide-wooden-chest.png",
-        icon_size = 128,
-      },
+      icon = "__wide-steel-chests__/graphics/icons/wide-wooden-chest.png",
+      icon_size = 128,
     },
-    subgroup = "wide-storage",
-    place_result = "wide-wooden-chest",
-    stack_size = 50,
-    order = "a"
   },
-   {
-    type = "container",
-    name = "wide-wooden-chest",
-    icons = 
-    {
-      {
-        icon = "__wide-steel-chests__/graphics/icons/wide-wooden-chest.png",
-        icon_size = 128,
-      },
-    },
-    flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 0.2, result = "wide-wooden-chest"},
-    max_health = 200,
-    corpse = "wide-wooden-chest-remnants",
-    dying_explosion = "wooden-chest-explosion",
-    open_sound = { filename = "__base__/sound/wooden-chest-open.ogg", volume = 0.6 },
-    close_sound = { filename = "__base__/sound/wooden-chest-close.ogg", volume = 0.6 },
-    collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
-    selection_box = {{-1, -0.5}, {1, 0.5}},
-    scale_info_icons = true,
-    fast_replaceable_group = "container",
-    inventory_size = 32,
-    vehicle_impact_sound = table.deepcopy(data.raw.container["wooden-chest"].vehicle_impact_sound),
-    damaged_trigger_effect = table.deepcopy(data.raw.container["wooden-chest"].damaged_trigger_effect),
-    picture =
+  collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
+  selection_box = {{-1, -0.5}, {1, 0.5}},
+  picture =
+  {
+    north =
     {
       layers =
       {
@@ -62,7 +34,28 @@ data:extend(
         }
       }
     },
-    circuit_connector = table.deepcopy(circuit_connector_definitions["wide-chest"]),
-    circuit_wire_max_distance = default_circuit_wire_max_distance,
+    east =
+    {
+      layers =
+      {
+        {
+          filename = "__wide-steel-chests__/graphics/entity/tall-wooden-chest.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 80,
+          height = 128,
+          shift = util.by_pixel(-2, 0.5),
+        },
+        {
+          filename = "__wide-steel-chests__/graphics/entity/shadows/tall-wooden-chest-shadow.png",
+          priority = "extra-high",
+          scale = 0.5,
+          width = 46,
+          height = 165,
+          shift = util.by_pixel(8, 21.25-10),
+          draw_as_shadow = true,
+        }
+      }
+    }
   }
-})
+}
