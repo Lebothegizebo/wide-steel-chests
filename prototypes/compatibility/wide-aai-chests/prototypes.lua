41ffc275beb053940c6b6b8e0f9188d4c202e70e
Lebothegizebo <daltonliam321@gmail.com>
2024-10-26 00:16:13 +1100

    Lebothegizebos brain melting...

--- prototypes/compatibility/wide-aai-chests/prototypes.lua
+++ prototypes/compatibility/wide-aai-chests/prototypes.lua
@@ -1,7 +1,7 @@
 --Loads all req
 require("scripts.compatibility.wide-aai-chests.sprites")
 
---wide-steel-chest--
+--MARK wide-steel-chest
 data.raw["container"]["wide-steel-chest"]["picture"].layers[1].width=128
 data.raw["container"]["wide-steel-chest"]["picture"].layers[1].height=74
 data.raw["container"]["wide-steel-chest"]["picture"].layers[2].width=102
@@ -33,6 +33,36 @@ data.raw["corpse"]["wide-steel-chest-remnants"].animation.layers[2].shift = util
 data.raw["corpse"]["wide-steel-chest-remnants"].animation.layers[2].direction_count = 1
 data.raw["corpse"]["wide-steel-chest-remnants"].animation.layers[2].draw_as_shadow = true
 
+-- MARK tall-steel-chest
+data.raw["container"]["tall-steel-chest"]["picture"].layers[1].width=64
+data.raw["container"]["tall-steel-chest"]["picture"].layers[1].height=148
+data.raw["container"]["tall-steel-chest"]["picture"].layers[2].width=102
+data.raw["container"]["tall-steel-chest"]["picture"].layers[2].height=148
+data.raw["container"]["tall-steel-chest"].icons[1].icon = "__wide-aai-chests__/graphics/icons/tall-steel-chest.png"
+data.raw["container"]["tall-steel-chest"]["picture"].layers[1].filename = "__wide-aai-chests__/graphics/entity/tall-steel-chest.png"
+data.raw["container"]["tall-steel-chest"]["picture"].layers[2].filename = "__wide-aai-chests__/graphics/entity/shadows/tall-steel-chest-shadow.png"
+--shift--
+data.raw["container"]["tall-steel-chest"]["picture"].layers[1].shift = util.by_pixel(0, 0)
+data.raw["container"]["tall-steel-chest"]["picture"].layers[2].shift = util.by_pixel(26, 3)
+--corpse--
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1] = {}
+data.raw["corpse"]["tall-steel-chest-remnants"].icons[1].icon = "__wide-aai-chests__/graphics/icons/wide-steel-chest.png"
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].width = 128
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].height = 74
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].scale = 0.5
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].filename = "__wide-aai-chests__/graphics/remnants/tall-steel-chest-remnants.png"
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].shift = util.by_pixel(0, -2)
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[1].direction_count = 1
+--corpse-shadow--
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2] = {}
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].filename = "__wide-aai-chests__/graphics/entity/shadows/tall-steel-chest-shadow.png"
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].width = 102
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].height = 50
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].scale = 0.5
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].shift = util.by_pixel(26, 3)
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].direction_count = 1
+data.raw["corpse"]["tall-steel-chest-remnants"].animation.layers[2].draw_as_shadow = true
+
 --technology--
 
 data.raw["technology"]["wide-steel-containers"].icons[1].icon="__wide-aai-chests__/graphics/technology/wide-steel-containers.png"
@@ -40,4 +70,5 @@ data.raw["technology"]["wide-steel-containers"].icons[1].icon="__wide-aai-chests
 --circuit connector sprites
 data.raw["container"]["wide-steel-chest"].circuit_connector.sprites = nil
 data.raw["container"]["wide-steel-chest"].circuit_connector.points = wide_aai_chest_circuit_connector_definitions.points
-
+data.raw["container"]["tall-steel-chest"].circuit_connector.sprites = nil
+data.raw["container"]["tall-steel-chest"].circuit_connector.points = wide_aai_chest_circuit_connector_definitions.points
--- scripts/lengthen_chests.lua
+++ scripts/lengthen_chests.lua
@@ -52,88 +52,91 @@ function make_wide_and_tall(params)
 
 	local localised_name = params.localised_name or {"entity-name."..wide_name}
 
-	--MARK: Containers
+--MARK: Containers
 
-	wide_container.name = wide_name
-	tall_container.name = tall_name
+wide_container.name = wide_name
+tall_container.name = tall_name
 
-	wide_container.subgroup = params.subgroup
-	wide_container.subgroup = params.subgroup
+wide_container.subgroup = params.subgroup
+wide_container.subgroup = params.subgroup
 
-	wide_container.icon = nil
-	wide_container.icons = params.icons
-	tall_container.icon = nil
-	tall_container.icons = params.icons
+wide_container.icon = nil
+wide_container.icons = params.icons
+tall_container.icon = nil
+tall_container.icons = params.icons
 
-	wide_container.minable.result = item_name
-	tall_container.minable.result = item_name
+wide_container.minable.result = item_name
+tall_container.minable.result = item_name
 
-	wide_container.corpse = wide_remnants_name
-	tall_container.corpse = tall_remnants_name
+wide_container.corpse = wide_remnants_name
+tall_container.corpse = tall_remnants_name
 
-	wide_container.collision_box = params.collision_box
-	wide_container.selection_box = params.selection_box
-	tall_container.collision_box = rotate_box(params.collision_box)
-	tall_container.selection_box = rotate_box(params.selection_box)
+wide_container.collision_box = params.collision_box
+wide_container.selection_box = params.selection_box
+tall_container.collision_box = rotate_box(params.collision_box)
+tall_container.selection_box = rotate_box(params.selection_box)
 
-	wide_container.inventory_size = wide_container.inventory_size * params.inventory_multiplier
-	tall_container.inventory_size = tall_container.inventory_size * params.inventory_multiplier
+wide_container.inventory_size = wide_container.inventory_size * params.inventory_multiplier
+tall_container.inventory_size = tall_container.inventory_size * params.inventory_multiplier
 
-	wide_container.picture = params.horizontal_picture
-	wide_container.animation = params.horizontal_animation
-	tall_container.picture = params.vertical_picture
-	tall_container.animation = params.horizontal_animation
+wide_container.picture = params.horizontal_picture
+wide_container.animation = params.horizontal_animation
+tall_container.picture = params.vertical_picture
+tall_container.animation = params.horizontal_animation
 
-	wide_container.circuit_connector = params.horizontal_connection or wide_container.circuit_connector
-	wide_container.circuit_connector = params.vertical_connection or tall_container.circuit_connector
+wide_container.circuit_connector = params.horizontal_connection or wide_container.circuit_connector
+wide_container.circuit_connector = params.vertical_connection or tall_container.circuit_connector
 
-	wide_container.placeable_by = {item = item_name, count = 1}
-	tall_container.placeable_by = {item = item_name, count = 1}
+wide_container.placeable_by = {item = item_name, count = 1}
+tall_container.placeable_by = {item = item_name, count = 1}
 
-	-- Hide it because we just want it to see the wide one
-	tall_container.hidden_in_factoriopedia = true
-	tall_container.localised_name = localised_name
+-- Hide it because we just want it to see the wide one
+tall_container.hidden_in_factoriopedia = true
+tall_container.hidden = true
+tall_container.localised_name = localised_name
 
-	--MARK: Remnants
+--MARK: Remnants
 
-	wide_remnants.name = wide_remnants_name
-	tall_remnants.name = tall_remnants_name
+wide_remnants.name = wide_remnants_name
+tall_remnants.name = tall_remnants_name
 
-	wide_remnants.subgroup = remnants_subgroup
-	tall_remnants.subgroup = remnants_subgroup
+wide_remnants.subgroup = remnants_subgroup
+tall_remnants.subgroup = remnants_subgroup
 
-	wide_remnants.icon = nil
-	wide_remnants.icons = params.icons
-	tall_remnants.icon = nil
-	tall_remnants.icons = params.icons
+wide_remnants.icon = nil
+wide_remnants.icons = params.icons
+tall_remnants.icon = nil
+tall_remnants.icons = params.icons
 
-	wide_remnants.selection_box = table.deepcopy(params.selection_box) -- Deepcopy to keep it from being linked to the container
-	tall_remnants.selection_box = rotate_box(params.selection_box)
+wide_remnants.selection_box = table.deepcopy(params.selection_box) -- Deepcopy to keep it from being linked to the container
+tall_remnants.selection_box = rotate_box(params.selection_box)
 
-	-- Why were these set, they are determined automatically..
-	wide_remnants.tile_width = nil
-	wide_remnants.tile_height = nil
-	tall_remnants.tile_width = nil
-	tall_remnants.tile_height = nil
+-- Why were these set, they are determined automatically..
+wide_remnants.tile_width = nil
+wide_remnants.tile_height = nil
+tall_remnants.tile_width = nil
+tall_remnants.tile_height = nil
 
-	wide_remnants.animation = params.horizontal_remnants
-	tall_remnants.animation = params.vertical_remnants
+wide_remnants.animation = params.horizontal_remnants
+tall_remnants.animation = params.vertical_remnants
 
-	wide_remnants.localised_name = {"remnant-name", localised_name}
-	tall_remnants.localised_name = {"remnant-name", localised_name}
+wide_remnants.localised_name = {"remnant-name", localised_name}
+tall_remnants.localised_name = {"remnant-name", localised_name}
 
-	--MARK: Item & ASM
+tall_remnants.hidden = true
 
-  data:extend{
-		{
-      type = "item",
-      name = item_name,
-      subgroup = "wide-storage",
-			flags = {"primary-place-result"},
-      place_result = rote_name,
-      stack_size = 50,
-      order = "b",
-      icons = params.icons
+--MARK: Item & ASM
+
+data:extend{
+	{
+	type = "item",
+	name = item_name,
+	subgroup = "wide-storage",
+	flags = {"primary-place-result"},
+	place_result = rote_name,
+	stack_size = 50,
+	order = "b",
+	icons = params.icons
 		}--[[@as data.ItemPrototype]],
 		{
 			type = "assembling-machine",
@@ -141,6 +144,7 @@ function make_wide_and_tall(params)
 			localised_name = localised_name,
 			icons = params.icons,
 			hidden_in_factoriopedia = true,
+			
 
 			-- Empty minable results is intentional
 			-- Item may be accidentally lost, but it's better than duplicating it
