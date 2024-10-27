9d766c72ba4a4cc4ce3428cbd5d10d4bec0aceb1
Lebothegizebo <daltonliam321@gmail.com>
2024-10-27 13:06:27 +1100

    commit

--- prototypes/beta-content.lua
+++ prototypes/beta-content.lua
@@ -1,9 +1,10 @@
-		--BETA CONTENT FLAGS
+--Beta Content Flags
 
-		if settings.startup["enable-wide-containers-beta"].value == false then
-            data.raw["technology"]["wide-storage-containers"].hidden=true
-            data.raw["technology"]["wide-logistics-containers"].hidden=true
-		else
-            data.raw["technology"]["wide-storage-containers"].hidden=false
-            data.raw["technology"]["wide-logistics-containers"].hidden=false
-		end
+--Prototypes
+if settings.startup["enable-wide-containers-beta"].value == false then
+      data.raw["technology"]["wide-storage-containers"].hidden=true
+      data.raw["technology"]["wide-logistics-containers"].hidden=true
+else
+      data.raw["technology"]["wide-storage-containers"].hidden=false
+      data.raw["technology"]["wide-logistics-containers"].hidden=false
+end
--- prototypes/entities/wide-steel-chest.lua
+++ prototypes/entities/wide-steel-chest.lua
@@ -12,6 +12,10 @@ make_wide_and_tall{
   inventory_multiplier = 2,
   collision_box = {{-0.8, -0.35}, {0.8, 0.35}},
   selection_box = {{-1, -0.5}, {1, 0.5}},
+  horizontal_connection = 
+  {
+    circuit_connector=circuit_connector_definitions["wide-chest"]
+  },
   horizontal_picture =
   {
     layers =
