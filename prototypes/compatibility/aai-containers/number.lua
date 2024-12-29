25b067c706313ca28ee5956857b759bfec95ecd7
Lebothegizebo <daltonliam321@gmail.com>
2024-12-29 23:33:24 +1100

    Updated Compatibillity

--- prototypes/compatibility/aai-containers/number.lua
+++ prototypes/compatibility/aai-containers/number.lua
@@ -4,16 +4,16 @@ local icon_size = 47
 --Initialize icon arrays
 data.raw["item"]["wide-steel-chest"].icons[2] = {}
 data.raw["recipe"]["wide-steel-chest"].icons[2] = {}
-data.raw["item"]["wide-passive-provider-chest"].icons[2] = {}
-data.raw["recipe"]["wide-passive-provider-chest"].icons[2] = {}
-data.raw["item"]["wide-active-provider-chest"].icons[2] = {}
-data.raw["recipe"]["wide-active-provider-chest"].icons[2] = {}
-data.raw["item"]["wide-storage-chest"].icons[2] = {}
-data.raw["recipe"]["wide-storage-chest"].icons[2] = {}
-data.raw["item"]["wide-buffer-chest"].icons[2] = {}
-data.raw["recipe"]["wide-buffer-chest"].icons[2] = {}
-data.raw["item"]["wide-requester-chest"].icons[2] = {}
-data.raw["recipe"]["wide-requester-chest"].icons[2] = {}
+data.raw["item"]["wide-passive-provider-chest"].icons[3] = {}
+data.raw["recipe"]["wide-passive-provider-chest"].icons[3] = {}
+data.raw["item"]["wide-active-provider-chest"].icons[3] = {}
+data.raw["recipe"]["wide-active-provider-chest"].icons[3] = {}
+data.raw["item"]["wide-storage-chest"].icons[3] = {}
+data.raw["recipe"]["wide-storage-chest"].icons[3] = {}
+data.raw["item"]["wide-buffer-chest"].icons[3] = {}
+data.raw["recipe"]["wide-buffer-chest"].icons[3] = {}
+data.raw["item"]["wide-requester-chest"].icons[3] = {}
+data.raw["recipe"]["wide-requester-chest"].icons[3] = {}
 
 --wide steel chest
 --item
@@ -29,65 +29,65 @@ data.raw["recipe"]["wide-steel-chest"].icons[2].shift = shift
 
 --wide passive-provider chest
 --item
-data.raw["item"]["wide-passive-provider-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["item"]["wide-passive-provider-chest"].icons[2].icon_size = icon_size
-data.raw["item"]["wide-passive-provider-chest"].icons[2].scale = scale
-data.raw["item"]["wide-passive-provider-chest"].icons[2].shift = shift
+data.raw["item"]["wide-passive-provider-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["item"]["wide-passive-provider-chest"].icons[3].icon_size = icon_size
+data.raw["item"]["wide-passive-provider-chest"].icons[3].scale = scale
+data.raw["item"]["wide-passive-provider-chest"].icons[3].shift = shift
 
 --recipe
-data.raw["recipe"]["wide-passive-provider-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["recipe"]["wide-passive-provider-chest"].icons[2].icon_size = icon_size
-data.raw["recipe"]["wide-passive-provider-chest"].icons[2].scale = scale
-data.raw["recipe"]["wide-passive-provider-chest"].icons[2].shift = shift
+data.raw["recipe"]["wide-passive-provider-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["recipe"]["wide-passive-provider-chest"].icons[3].icon_size = icon_size
+data.raw["recipe"]["wide-passive-provider-chest"].icons[3].scale = scale
+data.raw["recipe"]["wide-passive-provider-chest"].icons[3].shift = shift
 
 --wide active-provider chest
 --item
-data.raw["item"]["wide-active-provider-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["item"]["wide-active-provider-chest"].icons[2].icon_size = icon_size
-data.raw["item"]["wide-active-provider-chest"].icons[2].scale = scale
-data.raw["item"]["wide-active-provider-chest"].icons[2].shift = shift
+data.raw["item"]["wide-active-provider-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["item"]["wide-active-provider-chest"].icons[3].icon_size = icon_size
+data.raw["item"]["wide-active-provider-chest"].icons[3].scale = scale
+data.raw["item"]["wide-active-provider-chest"].icons[3].shift = shift
 
 --recipe
-data.raw["recipe"]["wide-active-provider-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["recipe"]["wide-active-provider-chest"].icons[2].icon_size = icon_size
-data.raw["recipe"]["wide-active-provider-chest"].icons[2].scale = scale
-data.raw["recipe"]["wide-active-provider-chest"].icons[2].shift = shift
+data.raw["recipe"]["wide-active-provider-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["recipe"]["wide-active-provider-chest"].icons[3].icon_size = icon_size
+data.raw["recipe"]["wide-active-provider-chest"].icons[3].scale = scale
+data.raw["recipe"]["wide-active-provider-chest"].icons[3].shift = shift
 
 --wide storage chest
 --item
-data.raw["item"]["wide-storage-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["item"]["wide-storage-chest"].icons[2].icon_size = icon_size
-data.raw["item"]["wide-storage-chest"].icons[2].scale = scale
-data.raw["item"]["wide-storage-chest"].icons[2].shift = shift
+data.raw["item"]["wide-storage-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["item"]["wide-storage-chest"].icons[3].icon_size = icon_size
+data.raw["item"]["wide-storage-chest"].icons[3].scale = scale
+data.raw["item"]["wide-storage-chest"].icons[3].shift = shift
 
 --recipe
-data.raw["recipe"]["wide-storage-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["recipe"]["wide-storage-chest"].icons[2].icon_size = icon_size
-data.raw["recipe"]["wide-storage-chest"].icons[2].scale = scale
-data.raw["recipe"]["wide-storage-chest"].icons[2].shift = shift
+data.raw["recipe"]["wide-storage-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["recipe"]["wide-storage-chest"].icons[3].icon_size = icon_size
+data.raw["recipe"]["wide-storage-chest"].icons[3].scale = scale
+data.raw["recipe"]["wide-storage-chest"].icons[3].shift = shift
 
 --wide buffer chest
 --item
-data.raw["item"]["wide-buffer-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["item"]["wide-buffer-chest"].icons[2].icon_size = icon_size
-data.raw["item"]["wide-buffer-chest"].icons[2].scale = scale
-data.raw["item"]["wide-buffer-chest"].icons[2].shift = shift
+data.raw["item"]["wide-buffer-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["item"]["wide-buffer-chest"].icons[3].icon_size = icon_size
+data.raw["item"]["wide-buffer-chest"].icons[3].scale = scale
+data.raw["item"]["wide-buffer-chest"].icons[3].shift = shift
 
 --recipe
-data.raw["recipe"]["wide-buffer-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["recipe"]["wide-buffer-chest"].icons[2].icon_size = icon_size
-data.raw["recipe"]["wide-buffer-chest"].icons[2].scale = scale
-data.raw["recipe"]["wide-buffer-chest"].icons[2].shift = shift
+data.raw["recipe"]["wide-buffer-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["recipe"]["wide-buffer-chest"].icons[3].icon_size = icon_size
+data.raw["recipe"]["wide-buffer-chest"].icons[3].scale = scale
+data.raw["recipe"]["wide-buffer-chest"].icons[3].shift = shift
 
 --wide requester chest
 --item
-data.raw["item"]["wide-requester-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["item"]["wide-requester-chest"].icons[2].icon_size = icon_size
-data.raw["item"]["wide-requester-chest"].icons[2].scale = scale
-data.raw["item"]["wide-requester-chest"].icons[2].shift = shift
+data.raw["item"]["wide-requester-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["item"]["wide-requester-chest"].icons[3].icon_size = icon_size
+data.raw["item"]["wide-requester-chest"].icons[3].scale = scale
+data.raw["item"]["wide-requester-chest"].icons[3].shift = shift
 
 --recipe
-data.raw["recipe"]["wide-requester-chest"].icons[2].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
-data.raw["recipe"]["wide-requester-chest"].icons[2].icon_size = icon_size
-data.raw["recipe"]["wide-requester-chest"].icons[2].scale = scale
-data.raw["recipe"]["wide-requester-chest"].icons[2].shift = shift
+data.raw["recipe"]["wide-requester-chest"].icons[3].icon = "__wide-aai-chests__/graphics/icons/number/1x2.png"
+data.raw["recipe"]["wide-requester-chest"].icons[3].icon_size = icon_size
+data.raw["recipe"]["wide-requester-chest"].icons[3].scale = scale
+data.raw["recipe"]["wide-requester-chest"].icons[3].shift = shift
