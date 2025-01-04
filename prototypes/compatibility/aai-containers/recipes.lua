86166eb7ab206033b3c795569b7645566fb00f4e
PennyJim <40438818+PennyJim@users.noreply.github.com>
2025-01-04 16:22:37 -0700

    Fix indentation on compatibility recipes.lua
    I'm not moving it to meld, because I'd say it's neatly organized as is.

--- prototypes/compatibility/aai-containers/recipes.lua
+++ prototypes/compatibility/aai-containers/recipes.lua
@@ -1,60 +1,59 @@
-    --Recipe--
-    
-    --Update Subgroups
+--Recipe--
 
-    --wide-storage
-    data.raw["recipe"]["wide-passive-provider-chest"].subgroup="wide-storage"
-    data.raw["recipe"]["wide-active-provider-chest"].subgroup="wide-storage"
-    data.raw["recipe"]["wide-storage-chest"].subgroup="wide-storage"
-    data.raw["recipe"]["wide-buffer-chest"].subgroup="wide-storage"
-    data.raw["recipe"]["wide-requester-chest"].subgroup="wide-storage"
+--Update Subgroups
 
-    --wide-aai-wood-containers
-    data.raw["recipe"]["iron-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["recipe"]["wooden-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["recipe"]["wide-iron-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["recipe"]["wide-wooden-chest"].subgroup="wide-aai-wood-containers"
+--wide-storage
+data.raw["recipe"]["wide-passive-provider-chest"].subgroup="wide-storage"
+data.raw["recipe"]["wide-active-provider-chest"].subgroup="wide-storage"
+data.raw["recipe"]["wide-storage-chest"].subgroup="wide-storage"
+data.raw["recipe"]["wide-buffer-chest"].subgroup="wide-storage"
+data.raw["recipe"]["wide-requester-chest"].subgroup="wide-storage"
 
-    --Update Recipe Order
+--wide-aai-wood-containers
+data.raw["recipe"]["iron-chest"].subgroup="wide-aai-wood-containers"
+data.raw["recipe"]["wooden-chest"].subgroup="wide-aai-wood-containers"
+data.raw["recipe"]["wide-iron-chest"].subgroup="wide-aai-wood-containers"
+data.raw["recipe"]["wide-wooden-chest"].subgroup="wide-aai-wood-containers"
 
-    data.raw["recipe"]["wide-steel-chest"].order = "a"
-    data.raw["recipe"]["wide-passive-provider-chest"].order = "b"
-    data.raw["recipe"]["wide-active-provider-chest"].order = "c"
-    data.raw["recipe"]["wide-storage-chest"].order = "d"
-    data.raw["recipe"]["wide-buffer-chest"].order = "e"
-    data.raw["recipe"]["wide-requester-chest"].order = "f"
-    data.raw["recipe"]["iron-chest"].order="aa"
-    data.raw["recipe"]["wooden-chest"].order="ab"
-    data.raw["recipe"]["wide-iron-chest"].order="ac"
-    data.raw["recipe"]["wide-wooden-chest"].order="ad"
-    
-    --Entity--
-    
-    --Update Subgroups
+--Update Recipe Order
 
-    --wide-storage
-    data.raw["item"]["wide-passive-provider-chest"].subgroup="wide-storage"
-    data.raw["item"]["wide-active-provider-chest"].subgroup="wide-storage"
-    data.raw["item"]["wide-storage-chest"].subgroup="wide-storage"
-    data.raw["item"]["wide-buffer-chest"].subgroup="wide-storage"
-    data.raw["item"]["wide-requester-chest"].subgroup="wide-storage"
-    
-    --wide-aai-wood-containers
-    data.raw["item"]["iron-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["item"]["wooden-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["item"]["wide-iron-chest"].subgroup="wide-aai-wood-containers"
-    data.raw["item"]["wide-wooden-chest"].subgroup="wide-aai-wood-containers"
+data.raw["recipe"]["wide-steel-chest"].order = "a"
+data.raw["recipe"]["wide-passive-provider-chest"].order = "b"
+data.raw["recipe"]["wide-active-provider-chest"].order = "c"
+data.raw["recipe"]["wide-storage-chest"].order = "d"
+data.raw["recipe"]["wide-buffer-chest"].order = "e"
+data.raw["recipe"]["wide-requester-chest"].order = "f"
+data.raw["recipe"]["iron-chest"].order="aa"
+data.raw["recipe"]["wooden-chest"].order="ab"
+data.raw["recipe"]["wide-iron-chest"].order="ac"
+data.raw["recipe"]["wide-wooden-chest"].order="ad"
 
-    --Update Entity Order
+--Entity--
 
-    data.raw["item"]["wide-steel-chest"].order = "a"
-    data.raw["item"]["wide-passive-provider-chest"].order = "b"
-    data.raw["item"]["wide-active-provider-chest"].order = "c"
-    data.raw["item"]["wide-storage-chest"].order = "d"
-    data.raw["item"]["wide-buffer-chest"].order = "e"
-    data.raw["item"]["wide-requester-chest"].order = "f"
-    data.raw["item"]["iron-chest"].order="aa"
-    data.raw["item"]["wooden-chest"].order="ab"
-    data.raw["item"]["wide-iron-chest"].order="ac"
-    data.raw["item"]["wide-wooden-chest"].order="ad"
-    
+--Update Subgroups
+
+--wide-storage
+data.raw["item"]["wide-passive-provider-chest"].subgroup="wide-storage"
+data.raw["item"]["wide-active-provider-chest"].subgroup="wide-storage"
+data.raw["item"]["wide-storage-chest"].subgroup="wide-storage"
+data.raw["item"]["wide-buffer-chest"].subgroup="wide-storage"
+data.raw["item"]["wide-requester-chest"].subgroup="wide-storage"
+
+--wide-aai-wood-containers
+data.raw["item"]["iron-chest"].subgroup="wide-aai-wood-containers"
+data.raw["item"]["wooden-chest"].subgroup="wide-aai-wood-containers"
+data.raw["item"]["wide-iron-chest"].subgroup="wide-aai-wood-containers"
+data.raw["item"]["wide-wooden-chest"].subgroup="wide-aai-wood-containers"
+
+--Update Entity Order
+
+data.raw["item"]["wide-steel-chest"].order = "a"
+data.raw["item"]["wide-passive-provider-chest"].order = "b"
+data.raw["item"]["wide-active-provider-chest"].order = "c"
+data.raw["item"]["wide-storage-chest"].order = "d"
+data.raw["item"]["wide-buffer-chest"].order = "e"
+data.raw["item"]["wide-requester-chest"].order = "f"
+data.raw["item"]["iron-chest"].order="aa"
+data.raw["item"]["wooden-chest"].order="ab"
+data.raw["item"]["wide-iron-chest"].order="ac"
+data.raw["item"]["wide-wooden-chest"].order="ad"
