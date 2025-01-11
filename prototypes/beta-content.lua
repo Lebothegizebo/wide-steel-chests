fcde601b0420bb206b9a4dcda635d227ce28e3bd
Lebothegizebo <daltonliam321@gmail.com>
2025-01-11 16:31:11 +1100

    Removed all graphics, updated changelog.

--- prototypes/beta-content.lua
+++ prototypes/beta-content.lua
@@ -1,51 +1,8 @@
 --Beta Content Flags
 --Prototypes
 if settings.startup["enable-wide-containers-beta"].value == false then
-      data.raw["technology"]["wide-storage-containers"].hidden=true
-      data.raw["technology"]["wide-logistics-containers"].hidden=true
-      data.raw["recipe"]["wide-active-provider-chest"].hidden=true
-      data.raw["recipe"]["wide-passive-provider-chest"].hidden=true
-      data.raw["recipe"]["wide-storage-chest"].hidden=true
-      data.raw["recipe"]["wide-buffer-chest"].hidden=true
-      data.raw["recipe"]["wide-requester-chest"].hidden=true
-      data.raw["recipe"]["wide-active-provider-chest"].hidden_in_factoriopedia=true
-      data.raw["recipe"]["wide-passive-provider-chest"].hidden_in_factoriopedia=true
-      data.raw["recipe"]["wide-storage-chest"].hidden_in_factoriopedia=true
-      data.raw["recipe"]["wide-buffer-chest"].hidden_in_factoriopedia=true
-      data.raw["recipe"]["wide-requester-chest"].hidden_in_factoriopedia=true
 
-      data.raw["item"]["wide-active-provider-chest"].hidden=true
-      data.raw["item"]["wide-passive-provider-chest"].hidden=true
-      data.raw["item"]["wide-storage-chest"].hidden=true
-      data.raw["item"]["wide-buffer-chest"].hidden=true
-      data.raw["item"]["wide-requester-chest"].hidden=true
-      data.raw["item"]["wide-active-provider-chest"].hidden_in_factoriopedia=true
-      data.raw["item"]["wide-passive-provider-chest"].hidden_in_factoriopedia=true
-      data.raw["item"]["wide-storage-chest"].hidden_in_factoriopedia=true
-      data.raw["item"]["wide-buffer-chest"].hidden_in_factoriopedia=true
-      data.raw["item"]["wide-requester-chest"].hidden_in_factoriopedia=true
 else
-      data.raw["technology"]["wide-storage-containers"].hidden=false
-      data.raw["technology"]["wide-logistics-containers"].hidden=false
-      data.raw["recipe"]["wide-active-provider-chest"].hidden=false
-      data.raw["recipe"]["wide-passive-provider-chest"].hidden=false
-      data.raw["recipe"]["wide-storage-chest"].hidden=false
-      data.raw["recipe"]["wide-buffer-chest"].hidden=false
-      data.raw["recipe"]["wide-requester-chest"].hidden=false
-      data.raw["recipe"]["wide-active-provider-chest"].hidden_in_factoriopedia=false
-      data.raw["recipe"]["wide-passive-provider-chest"].hidden_in_factoriopedia=false
-      data.raw["recipe"]["wide-storage-chest"].hidden_in_factoriopedia=false
-      data.raw["recipe"]["wide-buffer-chest"].hidden_in_factoriopedia=false
-      data.raw["recipe"]["wide-requester-chest"].hidden_in_factoriopedia=false
-
-      data.raw["item"]["wide-active-provider-chest"].hidden=false
-      data.raw["item"]["wide-passive-provider-chest"].hidden=false
-      data.raw["item"]["wide-storage-chest"].hidden=false
-      data.raw["item"]["wide-buffer-chest"].hidden=false
-      data.raw["item"]["wide-requester-chest"].hidden=false
-      data.raw["item"]["wide-active-provider-chest"].hidden_in_factoriopedia=false
-      data.raw["item"]["wide-passive-provider-chest"].hidden_in_factoriopedia=false
-      data.raw["item"]["wide-storage-chest"].hidden_in_factoriopedia=false
-      data.raw["item"]["wide-buffer-chest"].hidden_in_factoriopedia=false
-      data.raw["item"]["wide-requester-chest"].hidden_in_factoriopedia=false
+      log("Wide Containers has the beta setting enabled, but there is no beta version to load.")
+      print("Wide Containers has the beta setting enabled, but there is no beta version to load.")
 end
