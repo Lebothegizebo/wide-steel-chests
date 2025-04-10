fdb4c4eb04b04c236182c9064ced11dc14361213
Lebothegizebo <daltonliam321@gmail.com>
2025-04-10 20:48:09 +1000

    Removed Easter Egg Setting as it is unneeded in the 1.1 port

--- settings.lua
+++ settings.lua
@@ -1,21 +1,25 @@
-data:extend({
+if mods["wide-aai-chests"] then
+    data:extend(
     {
-        type = "bool-setting",
-        name = "wide-aai-chests-enabled-by-default",
-        setting_type = "startup",
-        default_value = true
-    },
-    {
-        type = "bool-setting",
-        name = "wide-aai-chests-enabled",
-        setting_type = "startup",
-        default_value = false
-    },
-    {
-        type = "bool-setting",
-        name = "enable-wide-containers-beta",
-        setting_type = "startup",
-        default_value = false
-    }
-    
-})
+        {
+            type = "bool-setting",
+            name = "wide-aai-chests-enabled-by-default",
+            setting_type = "startup",
+            default_value = true
+        },
+        {
+            type = "bool-setting",
+            name = "wide-aai-chests-enabled",
+            setting_type = "startup",
+            default_value = false
+        },
+    })
+    end
+    data:extend({
+        {
+            type = "bool-setting",
+            name = "enable-wide-containers-beta",
+            setting_type = "startup",
+            default_value = false
+        },
+    })
